462e5f9482fbe59e54ebad6dcba9e1d8d7bd7285
Rafael <rafagazani@gmail.com>
2023-03-28 15:24:52 -0400

    payload is Json string

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:csv/csv.dart';
 import 'package:csv_to_documents/server_appwrite/server_appwrite.dart';
 
@@ -6,35 +7,46 @@ import 'package:csv_to_documents/server_appwrite/server_appwrite.dart';
 
 Future start(final request, final response) async {
   try {
-    await ServerAppwrite.instance
-        .main(
-        request.variables['API_ENDPOINT'], request.variables['API_PROJECT_ID'],
-        request.variables['API_KEY']);
+    await ServerAppwrite.instance.main(request.variables['API_ENDPOINT'],
+        request.variables['API_PROJECT_ID'], request.variables['API_KEY']);
 
-    var payload = request.payload;
+    var payload = json.decode(request.payload);
 
     var csv = await ServerAppwrite.instance.storage.getFileDownload(
         bucketId: payload['storageId'], fileId: payload['fileId']);
-    final csvString = String.fromCharCodes(csv);
+    String csvString = '';
+    try {
+      csvString = String.fromCharCodes(csv);
+    } catch (e) {
+      return response.json({"csvString": "${e.toString()}"});
+    }
 
-    final List<List<dynamic>> csvTable = CsvToListConverter().convert(
-        csvString);
+    List<List<dynamic>> csvTable = [];
+    try {
+      csvTable = CsvToListConverter().convert(csvString);
+    } catch (e) {
+      return response.json({"CsvToListConverter": "${e.toString()}"});
+    }
 
-    for (final csvRow in csvTable) {
-      final document = await ServerAppwrite.instance.databases.createDocument(
-        databaseId: payload['databaseId'],
-        collectionId: payload['collectionId'],
-        documentId: 'unique()',
-        data: {
-          "churchId": csvRow[0],
-          "title": csvRow[1],
-          "module": csvRow[2],
-        },
-      );
+    try {
+      for (final csvRow in csvTable) {
+        final document = await ServerAppwrite.instance.databases.createDocument(
+          databaseId: payload['databaseId'],
+          collectionId: payload['collectionId'],
+          documentId: 'unique()',
+          data: {
+            "churchId": csvRow[0],
+            "title": csvRow[1],
+            "module": csvRow[2],
+          },
+        );
+      }
+    }catch (e) {
+      return response.json({"csvRow": "${e.toString()}"});
     }
 
     return response.json({"total": "${csvTable.length}"});
-  }catch (e){
+  } catch (e) {
     return response.json({"error": "${e.toString()}"});
   }
 }
