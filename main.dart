d6ad07b840043b8e21b8d28f4f14c5eca98d9a3b
Rafael <rafagazani@gmail.com>
2023-03-28 14:56:48 -0400

    try

--- main.dart
+++ main.dart
@@ -1,34 +1,40 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:csv/csv.dart';
 import 'package:csv_to_documents/server_appwrite/server_appwrite.dart';
 
 //appwrite functions createDeployment --functionId=csv_to_documents --activate=true --entrypoint="main.dart" --code="."
 
 Future start(final request, final response) async {
-  await ServerAppwrite.instance
-      .main(request.env['API_ENDPOINT'], request.env['API_PROJECT_ID'], request.env['API_KEY']);
+  try {
+    await ServerAppwrite.instance
+        .main(
+        request.variables['API_ENDPOINT'], request.variables['API_PROJECT_ID'],
+        request.variables['API_KEY']);
 
-  var payload = request.payload;
+    var payload = request.payload;
 
-  var csv = await ServerAppwrite.instance.storage.getFileDownload(
-      bucketId: payload['storageId'], fileId: payload['fileId']);
-  final csvString = String.fromCharCodes(csv);
+    var csv = await ServerAppwrite.instance.storage.getFileDownload(
+        bucketId: payload['storageId'], fileId: payload['fileId']);
+    final csvString = String.fromCharCodes(csv);
 
-  final List<List<dynamic>> csvTable = CsvToListConverter().convert(csvString);
+    final List<List<dynamic>> csvTable = CsvToListConverter().convert(
+        csvString);
 
-  for (final csvRow in csvTable) {
-    final document = await ServerAppwrite.instance.databases.createDocument(
-      databaseId: payload['databaseId'],
-      collectionId: payload['collectionId'],
-      documentId: 'unique()',
-      data: {
-        "churchId": csvRow[0],
-        "title": csvRow[1],
-        "module": csvRow[2],
-      },
-    );
+    for (final csvRow in csvTable) {
+      final document = await ServerAppwrite.instance.databases.createDocument(
+        databaseId: payload['databaseId'],
+        collectionId: payload['collectionId'],
+        documentId: 'unique()',
+        data: {
+          "churchId": csvRow[0],
+          "title": csvRow[1],
+          "module": csvRow[2],
+        },
+      );
+    }
+
+    return response.json({"total": "${csvTable.length}"});
+  }catch (e){
+    return response.json({"error": "${e.toString()}"});
   }
-
-  return response.json({"total": "${csvTable.length}"});
 }
